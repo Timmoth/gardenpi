f665e17455177476d1466c0cd9e5d4e68c44e594
timmoth <t.jones@timmoth.com>
2023-04-17 19:51:49 +0100

    updated video stitcher

--- scripts/createVideo.py
+++ scripts/createVideo.py
@@ -8,13 +8,13 @@ images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
 
-video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+video = cv2.VideoWriter(video_name, 0, 15, (width,height))
 frameCount = images.count()
 print(f"frames: '{frameCount}'")
 
 frameIndex = 0
-for image in images:
-    print(f"stitching frame: '{frameIndex}'")
+for image in images.sort():
+    print(f"stitching frame: '{image}' '{frameIndex}'")
     frameIndex += 1
     video.write(cv2.imread(os.path.join(image_folder, image)))
 
