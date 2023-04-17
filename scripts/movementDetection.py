72d6a68456b90ec65a196585cc6970e5c50ac59d
timmoth <t.jones@timmoth.com>
2023-04-17 20:25:14 +0100

    updated video stitcher

--- scripts/movementDetection.py
+++ scripts/movementDetection.py
@@ -6,11 +6,10 @@ cap = cv2.VideoCapture(0)  # video capture source camera (Here webcam of laptop)
 
 ret, frame1 = cap.read()
 
-oldFrame = frame1
 while(True):
     ret, frame2 = cap.read()
   
-    diff = cv2.absdiff(oldFrame, frame2)
+    diff = cv2.absdiff(frame1, frame2)
     diff_gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     diff_blur = cv2.GaussianBlur(diff_gray, (5,5), 0)
     _, thresh_bin = cv2.threshold(diff_blur, 20, 255, cv2.THRESH_BINARY)
@@ -22,7 +21,7 @@ while(True):
       timestr = time.strftime("%Y%m%d-%H%M%S")
       cv2.imwrite(f"{timestr}.jpg", frame2)   
     
-    oldFrame = frame2
+    frame1 = frame2
     time.sleep(0.5)
     # the 'q' button is set as the
     # quitting button you may use any
