f4f4748faafc795365bedacf92a7e311e3208fe8
timmoth <t.jones@timmoth.com>
2023-04-17 20:32:18 +0100

    updated video stitcher

--- scripts/movementDetection.py
+++ scripts/movementDetection.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)  # video capture source camera (Here webcam of laptop)
 
 ret, frame1 = cap.read()
 
-oldFrame = frame1.copy()
+oldFrame = frame1
 
 while(True):
     ret, frame2 = cap.read()
@@ -15,6 +15,7 @@ while(True):
     diff_gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     diff_blur = cv2.GaussianBlur(diff_gray, (5,5), 0)
     _, thresh_bin = cv2.threshold(diff_blur, 20, 255, cv2.THRESH_BINARY)
+    oldFrame = frame2
 
     number_of_white_pix = np.sum(thresh_bin == 255)
     print(number_of_white_pix)
@@ -24,4 +25,3 @@ while(True):
       cv2.imwrite(f"{timestr}.jpg", frame2)   
       time.sleep(0.5)
       print(f"Write: {timestr}")
-      oldFrame = frame2.copy()
