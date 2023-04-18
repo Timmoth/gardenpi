485974d4c1b09dd21e13e986e97a7682f0e794a0
timmoth <t.jones@timmoth.com>
2023-04-18 20:58:44 +0100

    updated createVideo

--- scripts/createVideo.py
+++ scripts/createVideo.py
@@ -1,11 +1,17 @@
 import cv2
 import os
+import argparse
 
-image_folder = './'
+parser = argparse.ArgumentParser()
+parser.add_argument("--dir", action="store_true",
+                    help="image dir")
+args = parser.parse_args()
+imageDir = args.imageDir
+
 video_name = 'video.mp4'
 
-images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
-frame = cv2.imread(os.path.join(image_folder, images[0]))
+images = [img for img in os.listdir(imageDir) if img.endswith(".jpg")]
+frame = cv2.imread(os.path.join(imageDir, images[0]))
 height, width, layers = frame.shape
 
 fourcc = cv2.VideoWriter_fourcc(*'h264')
@@ -18,7 +24,7 @@ sortedFrames = sorted(images)
 for image in sortedFrames:
     print(f"stitching frame: '{image}' '{frameIndex}'")
     frameIndex += 1
-    video.write(cv2.imread(os.path.join(image_folder, image)))
+    video.write(cv2.imread(os.path.join(imageDir, image)))
 
 cv2.destroyAllWindows()
 video.release()
